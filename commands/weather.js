ade31e0a1bf21467897f895a215e5a3315b460b8
NoWay_y <teolem77@gmail.com>
2022-06-24 01:07:01 +0200

    bot working with sentences

--- commands/weather.js
+++ commands/weather.js
@@ -1,6 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const fetch = require('node-fetch');
-const { MessageEmbed } = require('discord.js');
 
 async function getJSONResponse(body) {
   let fullBody = '';
@@ -17,24 +16,39 @@ module.exports = {
     .setName("weather")
     .setDescription("Gives the weather of the specified date if given, else of the current day")
     .addStringOption(option =>
-      option.setName('location')
-        .setDescription("Gives the weather in this location")
+      option.setName("location")
+        .setDescription("The location of the wanted weather")
         .setRequired(true)),
+
   async execute(interaction) {
 
+
     let searchedLocation = interaction.options.getString('location');
+    console.log(searchedLocation);
 
     let url = `https://api.weatherapi.com/v1/current.json?key=afc7001b47b34cba8e2170548221106&q=${searchedLocation}`;
-    let settings = { method: "GET"};
+    let settings = { method: "GET" };
 
-    let response = new MessageEmbed();
+    let response = "";
 
     await fetch(url, settings)
       .then(res => res.json())
-      .then(data => {
-        response.setTitle(`Weather of ${Date.now()}`)
-          .setAuthor({ name: "NoWay_y", iconURL: ''})
+      .then(async data => {
+        console.log(data);
+
+        let country = data["location"].country;
+        let region = data["location"].region;
+        let city = data["location"].name;
+
+        response += `Weather in ${city}, ${region}, ${country} : `
+        response += data["current"]["condition"].text;
+
       });
+
+    interaction.reply(response);
 
   }
 }
+
+// TODO: MessageEmbed with -> map, wind rose + wind speed, humidity + precipitation rate, temperature, weather icon,
+//  date, location (country, region, city)
